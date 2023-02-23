d69c429dd50ee2f136d422f5298749e290ec68ce
Alena Barinova <alena.barinova0411@gmail.com>
2023-02-23 08:36:10 +0300

    Tasks 1,2,3,4

---
+++ src/Main.java
@@ -0,0 +1,68 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+    }
+
+    private static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+        int[] arr = generateRandomArray();
+        System.out.println(Arrays.toString(arr));
+        int min = arr[0];
+        int max = arr[0];
+        for (int val : arr) {
+            System.out.println(val);
+            if (val < min) {
+                min = val;
+            }
+            if (val > max) {
+                max = val;
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей." +
+                "Максимальная сумма трат за день составила" + max + " рублей.");
+    }
+
+    public static void task3 () {
+        System.out.println("Задача 3");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        double average = (double) sum / arr.length;
+        System.out.println("Средняя сумма трат за месяц составила " + average +" рублей");
+    }
+
+    public static void task4 () {
+        System.out.println("Задача 4");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length -1; i >= 0; i--) {
+            System.out.println(reverseFullName[i]);
+        }
+    }
+
+}
